2959add36d96a9d99ea2ccdea67693b2cebb74f8
Gansukh23 <63278183+Gansukh23@users.noreply.github.com>
2020-08-04 10:34:36 +0800

    finished

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var scores=[0,0];
 var roundScore = 0;
 // Шооны аль талаарай буухыг хадгалах хувьсагч хэрэгтэйь 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
 var diceNumber = Math.floor(Math.random() * 6) + 1;
-
+//  Тоглоом дууссан эсэхийг хадгалах төлөвийн хувьсагч
+var isGameOver=true;
 
 //Програм эхлэхэд бэлтгэе.
 document.getElementById('score-0').textContent =scores[0];
@@ -21,7 +22,8 @@ diceDom.style.display= "none";
 
 // Шоог шидэх эвент листенер
 document.querySelector('.btn-roll').addEventListener("click", function(){
-    // санамсаргүй тоо гаргах 1 - 6
+    if(isGameOver){
+        // санамсаргүй тоо гаргах 1 - 6
     var diceNumber = Math.floor(Math.random() * 6) + 1;
     diceDom.style.display= "block";
     // буусан шооны зургийг гаргах
@@ -36,25 +38,33 @@ document.querySelector('.btn-roll').addEventListener("click", function(){
         //1тэй тэнцүү бол тоглогчийн ээлжийн оноог энэ хэсэгт солж өгнө. 
         switchToNextPlayer();
     }
+    }else{
+    alert("Тоглоом дууссан байна. NEW GAME товчийг дарж шинээр эхэлнэ үү");
+    }
 
     
 
 });
 
 document.querySelector('.btn-hold').addEventListener("click", function(){
-    scores[activePlayer] += roundScore;
+    if(isGameOver){
+        scores[activePlayer] += roundScore;
     
     document.getElementById('score-' + activePlayer).textContent =scores[activePlayer];
 
     
-    if(scores[activePlayer] >= 10){
+    if(scores[activePlayer] >= 100){
         //ялагч гэсэн текстийг нэрний оронд гарна.
+        isGameOver = false;
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
          document.getElementById("name-" + activePlayer).textContent = "Winner!!!";
+
     } else
     switchToNextPlayer();
-    
+    }else{
+    alert("Тоглоом дууссан байна. NEW GAME товчийг дарж шинээр эхэлнэ үү");
+    }
 })
 
 function switchToNextPlayer(){
@@ -77,6 +87,7 @@ function switchToNextPlayer(){
 }
 // шинэ тоглоом эхлүүлэх товч
 document.querySelector('.btn-new').addEventListener("click",function(){
+    isGameOver=true;
     scores = [0,0]
     roundScore=0;
     document.getElementById('score-0').textContent =scores[0];
